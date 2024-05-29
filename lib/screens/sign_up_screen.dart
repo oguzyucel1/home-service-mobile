714ee33a528d44848e70872ce4393bd794367227
10Abdullatif35 <147412229+10Abdullatif35@users.noreply.github.com>
2024-05-29 10:31:33 +0300

    user auth established in sign up

--- lib/screens/sign_up_screen.dart
+++ lib/screens/sign_up_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:home_hub/screens/splash_screen.dart';
+import 'package:supabase/supabase.dart'; // Import Supabase SDK
 import 'package:home_hub/main.dart';
 import 'package:home_hub/screens/otp_verification_screen.dart';
 import 'package:home_hub/screens/sign_in_screen.dart';
 import 'package:home_hub/utils/constant.dart';
 import 'package:home_hub/utils/widgets.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../custom_widget/space.dart';
 import '../utils/colors.dart';
@@ -19,34 +22,43 @@ class SignUpScreen extends StatefulWidget {
 class _SignUpScreenState extends State<SignUpScreen> {
   final _signUpFormKey = GlobalKey<FormState>();
 
-  final TextEditingController _passController = TextEditingController();
-  final TextEditingController _confirmPassController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _mobileController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
 
-  bool agreeWithTeams = false;
-  bool _securePassword = true;
-  bool _secureConfirmPassword = true;
+  bool _agreeWithTerms = false;
 
-  double screenHeight = 0.0;
-  double screenWidth = 0.0;
+  final supabase = Supabase.instance.client;
 
-  bool? checkBoxValue = false;
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   void dispose() {
-    _passController.dispose();
-    _confirmPassController.dispose();
+    _usernameController.dispose();
+    _emailController.dispose();
+    _mobileController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
     super.dispose();
   }
 
-  Future<void> _showAlertDialog() async {
+  Future<void> _showAlertDialog(BuildContext context, String message) async {
     return showDialog<void>(
       context: context,
-      barrierDismissible: false, // user must tap button!
+      barrierDismissible: false,
       builder: (BuildContext context) {
         return AlertDialog(
           title: Text('Alert'),
           content: SingleChildScrollView(
-            child: ListBody(children: [Text('Please agree the terms and conditions')]),
+            child: ListBody(
+              children: [Text(message)],
+            ),
           ),
           actions: [
             TextButton(
@@ -61,14 +73,42 @@ class _SignUpScreenState extends State<SignUpScreen> {
     );
   }
 
+  Future<void> _signUp() async {
+    final username = _usernameController.text.trim();
+    final email = _emailController.text.trim();
+    final mobile = _mobileController.text.trim();
+    final password = _passwordController.text.trim();
+    final confirmPassword = _confirmPasswordController.text.trim();
+
+    if (!_agreeWithTerms) {
+      _showAlertDialog(context, 'Please agree to the terms and conditions');
+      return;
+    }
+
+    if (password != confirmPassword) {
+      _showAlertDialog(context, 'Passwords do not match');
+      return;
+    }
+
+    final response =
+        await supabase.auth.signUp(email: email, password: password);
+    if (response == true) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => SplashScreen()),
+      );
+    } else {
+      // Sign-up successful, navigate to OTP verification screen or any other screen
+
+      var error;
+      _showAlertDialog(context, error);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    screenHeight = MediaQuery.of(context).size.height;
-    screenWidth = MediaQuery.of(context).size.width;
-
     return Scaffold(
       body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
         child: Padding(
           padding: const EdgeInsets.all(20.0),
           child: Column(
@@ -78,7 +118,10 @@ class _SignUpScreenState extends State<SignUpScreen> {
               Center(
                 child: Text(
                   "Sign Up",
-                  style: TextStyle(fontSize: mainTitleTextSize, fontWeight: FontWeight.bold),
+                  style: TextStyle(
+                    fontSize: mainTitleTextSize,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
               Space(60),
@@ -88,6 +131,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     TextFormField(
+                      controller: _usernameController,
                       keyboardType: TextInputType.name,
                       textInputAction: TextInputAction.next,
                       style: TextStyle(fontSize: 16),
@@ -95,6 +139,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
                     ),
                     Space(16),
                     TextFormField(
+                      controller: _emailController,
                       keyboardType: TextInputType.emailAddress,
                       textInputAction: TextInputAction.next,
                       style: TextStyle(fontSize: 20),
@@ -102,68 +147,58 @@ class _SignUpScreenState extends State<SignUpScreen> {
                     ),
                     Space(16),
                     TextFormField(
+                      controller: _mobileController,
                       keyboardType: TextInputType.phone,
                       textInputAction: TextInputAction.next,
                       inputFormatters: [LengthLimitingTextInputFormatter(10)],
                       style: TextStyle(fontSize: 20),
-                      decoration: commonInputDecoration(hintText: "Mobile Number"),
+                      decoration:
+                          commonInputDecoration(hintText: "Mobile Number"),
                     ),
                     Space(16),
                     TextFormField(
-                      controller: _passController,
+                      controller: _passwordController,
                       textInputAction: TextInputAction.next,
                       keyboardType: TextInputType.visiblePassword,
-                      obscureText: _securePassword,
+                      obscureText: true,
                       style: TextStyle(fontSize: 20),
                       decoration: commonInputDecoration(
                         hintText: "Password",
-                        suffixIcon: Padding(
-                          padding: EdgeInsets.only(right: 5.0),
-                          child: IconButton(
-                            icon: Icon(_securePassword ? Icons.visibility_off : Icons.visibility, size: 18),
-                            onPressed: () {
-                              _securePassword = !_securePassword;
-                              setState(() {});
-                            },
-                          ),
-                        ),
                       ),
                     ),
                     Space(16),
                     TextFormField(
-                      controller: _confirmPassController,
+                      controller: _confirmPasswordController,
                       textInputAction: TextInputAction.done,
                       autovalidateMode: AutovalidateMode.onUserInteraction,
                       keyboardType: TextInputType.emailAddress,
-                      obscureText: _secureConfirmPassword,
+                      obscureText: true,
                       style: TextStyle(fontSize: 20),
                       decoration: commonInputDecoration(
                         hintText: "Re-enter Password",
-                        suffixIcon: Padding(
-                          padding: EdgeInsets.only(right: 5.0),
-                          child: IconButton(
-                            icon: Icon(_secureConfirmPassword ? Icons.visibility_off : Icons.visibility, size: 18),
-                            onPressed: () {
-                              _secureConfirmPassword = !_secureConfirmPassword;
-                              setState(() {});
-                            },
-                          ),
-                        ),
                       ),
                     ),
                     Space(16),
                     Theme(
-                      data: ThemeData(unselectedWidgetColor: appData.isDark ? Colors.white : blackColor),
+                      data: ThemeData(
+                        unselectedWidgetColor:
+                            appData.isDark ? Colors.white : blackColor,
+                      ),
                       child: CheckboxListTile(
                         contentPadding: EdgeInsets.all(0),
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
                         activeColor: Colors.black,
-                        title: Text("I agree to the Terms and Conditions", style: TextStyle(fontWeight: FontWeight.normal)),
-                        value: checkBoxValue,
+                        title: Text(
+                          "I agree to the Terms and Conditions",
+                          style: TextStyle(fontWeight: FontWeight.normal),
+                        ),
+                        value: _agreeWithTerms,
                         dense: true,
                         onChanged: (newValue) {
                           setState(() {
-                            checkBoxValue = newValue;
+                            _agreeWithTerms = newValue!;
                           });
                         },
                         controlAffinity: ListTileControlAffinity.leading,
@@ -177,37 +212,42 @@ class _SignUpScreenState extends State<SignUpScreen> {
                           padding: EdgeInsets.all(16),
                           textStyle: TextStyle(fontSize: 25),
                           shape: StadiumBorder(),
-                          backgroundColor: appData.isDark ? Colors.grey.withOpacity(0.2) : Colors.black,
+                          backgroundColor: appData.isDark
+                              ? Colors.grey.withOpacity(0.2)
+                              : Colors.black,
                         ),
-                        onPressed: () {
-                          if (_signUpFormKey.currentState!.validate()) {
-                            if (agreeWithTeams == true) {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => OTPVerificationScreen()),
-                              );
-                            } else {
-                              _showAlertDialog();
-                            }
-                          }
-                        },
-                        child: Text("Sign Up", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: Colors.white)),
+                        onPressed: _signUp,
+                        child: Text(
+                          "Sign Up",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                            color: Colors.white,
+                          ),
+                        ),
                       ),
                     ),
                     Space(20),
                     GestureDetector(
                       onTap: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => SignInScreen()));
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => SignInScreen()),
+                        );
                       },
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Text("Have an account?", style: TextStyle(fontSize: 16)),
+                          Text("Have an account?",
+                              style: TextStyle(fontSize: 16)),
                           Space(4),
-                          Text('Sign In', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+                          Text('Sign In',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 16)),
                         ],
                       ),
-                    )
+                    ),
                   ],
                 ),
               ),
